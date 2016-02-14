ebc541ca73e4d2c2c504d302e69190a76570ce37
Takahiko Horiuchi <mogusa@gmail.com>
2016-02-14 09:10:16 +0000

    Generate private key QR codes.

--- generator.js
+++ generator.js
@@ -4,18 +4,25 @@ var fs = require('fs');
 var sprintf = require('sprintf-js').sprintf;
 
 fs.mkdirSync('images');
+fs.mkdirSync('images/public');
+fs.mkdirSync('images/private');
+
+var result = 'images/result.csv';
 
 for (var i=1; i<101; i++) {
   var serial_num = sprintf('%05d', i);
   var privateKey = new bitcore.PrivateKey();
   var address = privateKey.toAddress();
-  console.log(serial_num + ', ' + address.toString() + ', ' + privateKey.toString());
-  generate(address, serial_num);
+  var log = serial_num + ', ' + address.toString() + ', ' + privateKey.toString();
+  console.log(log);
+  fs.appendFile(result, log + "\n", function(err) {});
+  draw(address.toString(), 'images/public/' + serial_num + '.png');
+  draw(privateKey.toString(), 'images/private/' + serial_num + '.png');
 }
 
-function generate(address, serial_num) {
-  QRCode.draw(address.toString(), function(error, canvas) {
-    var out = fs.createWriteStream('images/qrcode_' + serial_num + '.png');
+function draw(string, path) {
+  QRCode.draw(string, function(error, canvas) {
+    var out = fs.createWriteStream(path);
     var stream = canvas.pngStream();
     stream.on('data', function(chunk) {
       out.write(chunk);
